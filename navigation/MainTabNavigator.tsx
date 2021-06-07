994665028c066f79a63917d7ab809bc09cf9252b
lakshanamal <lakshanamal100@gmail.com>
2021-06-07 13:16:42 +0530

    bug fixed

--- navigation/MainTabNavigator.tsx
+++ navigation/MainTabNavigator.tsx
@@ -5,6 +5,7 @@
 
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
@@ -14,7 +15,7 @@ import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
 
-const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+const BottomTab = createMaterialTopTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
   const colorScheme = useColorScheme();
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,17 +3,15 @@ import {
   DefaultTheme,
   DarkTheme,
 } from "@react-navigation/native";
-import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
-import { ColorSchemeName } from "react-native";
-import { View } from "../components/Themed";
+import { ColorSchemeName, View } from "react-native";
 import Colors from "../constants/Colors";
-import { Octicons, MaterialCommunityIcons } from "@expo/vector-icons";
-
 import NotFoundScreen from "../screens/NotFoundScreen";
 import { RootStackParamList } from "../types";
-import BottomTabNavigator from "./BottomTabNavigator";
+import BottomTabNavigator from "./MainTabNavigator";
 import LinkingConfiguration from "./LinkingConfiguration";
+import { Octicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Navigation({
   colorScheme,
@@ -30,11 +28,8 @@ export default function Navigation({
   );
 }
 
-// A root stack navigator is often used for displaying modals on top of all other content
-// Read more here: https://reactnavigation.org/docs/modal
 const Stack = createStackNavigator<RootStackParamList>();
 
-// meke thamai appbar eka flutter eke wage
 function RootNavigator() {
   return (
     <Stack.Navigator
@@ -42,8 +37,6 @@ function RootNavigator() {
         headerStyle: {
           backgroundColor: Colors.light.tint,
         },
-        headerTintColor: Colors.light.background,
-        headerTitleAlign: "left",
         headerTitleStyle: {
           fontWeight: "bold",
         },
@@ -55,21 +48,23 @@ function RootNavigator() {
         options={{
           title: "Whatsapp",
           headerRight: () => {
-           return (
-              <View style={{backgroundColor:Colors.light.tint,
-              flexDirection:"row" ,
-              width:60,
-              justifyContent:'space-around',
-              marginRight:10
-              }}>
-                <Octicons name="search" size={22} color={"white"} />
+            return (
+              <View
+                style={{
+                  flexDirection: "row",
+                  width: 60,
+                  justifyContent: "space-around",
+                  marginRight: 10,
+                }}
+              >
+                <Octicons name="search" size={21} color="white" />
                 <MaterialCommunityIcons
                   name="dots-vertical"
-                  size={24}
-                  color={"white"}
+                  size={22}
+                  color="white"
                 />
               </View>
-           )
+            );
           },
         }}
       />
