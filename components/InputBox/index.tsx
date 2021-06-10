e72c467449b01611a3676a4046bfb60e084d287f
lakshanamal <lakshanamal100@gmail.com>
2021-06-10 09:17:48 +0530

    send button config

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -2,9 +2,10 @@ import {
   Entypo,
   FontAwesome5,
   MaterialCommunityIcons,
+  MaterialIcons,
 } from "@expo/vector-icons";
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
 import { Text, TextInput } from "react-native";
 import { Message } from "../../types";
 import { View } from "../Themed";
@@ -15,23 +16,28 @@ import styles from "./style";
 // };
 
 const InputBox = () => {
-  //   const { message } = props;
+  const [message, setMessage] = useState("");
 
-  //   const isMyMessage = () => {
-  //     return message.user.id === "u1";
-  //   };
   return (
     <View style={styles.container}>
       <View style={styles.mainContainer}>
         <FontAwesome5 name="laugh-beam" size={24} color="gray" />
-        <TextInput style={styles.textInput} multiline numberOfLines={6} />
+        <TextInput
+          style={styles.textInput}
+          multiline
+          value={message}
+          onChangeText={setMessage}
+          placeholder={"Type a message ..."}
+        />
         <Entypo name="attachment" size={24} color="gray" style={styles.icon} />
         <Entypo name="camera" size={24} color="gray" style={styles.icon} />
-        {/* {!isMyMessage() && <Text style={styles.name}>{message.user.name}</Text>}
-        <Text style={styles.message}>{message.content}</Text> */}
       </View>
       <View style={styles.buttonController}>
-        <MaterialCommunityIcons name="microphone" size={24} color="white" />
+        {message ? (
+          <MaterialIcons name="send" size={24} color={"white"} />
+        ) : (
+          <MaterialCommunityIcons name="microphone" size={24} color="white" />
+        )}
       </View>
     </View>
   );
