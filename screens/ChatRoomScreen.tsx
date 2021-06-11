6e1f2620d8114ca78283d39373c30f5b3e8f76f7
lakshanamal <lakshanamal100@gmail.com>
2021-06-11 23:55:05 +0530

    database configuration done

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "../components/Themed";
 import { useRoute } from "@react-navigation/native";
 import ChatRoomData from "../data/Chats";
@@ -6,9 +6,15 @@ import { FlatList, ImageBackground } from "react-native";
 import ChatMessage from "../components/ChatMessage";
 import BG from "../assets/images/BG.png";
 import InputBox from "../components/InputBox";
+import {User} from "../firebaseConfig"
 
 const ChatRoomScreen = () => {
   const route = useRoute();
+
+  // const [users,setUsers]=useState([]);
+
+  console.log(User)
+
   return (
     <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
       <FlatList
