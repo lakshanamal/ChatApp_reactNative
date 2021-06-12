611f69326e9dc67dae0fd9fb139d721fefdfd808
lakshanamal <lakshanamal100@gmail.com>
2021-06-12 23:59:34 +0530

    splash added

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -11,24 +11,24 @@ import { isTSEnumMember } from "@babel/types";
 
 const ChatRoomScreen = () => {
   const route = useRoute();
-  const [data, setData] = [];
+  // const [data, setData] = [];
 
   // const [users,setUsers]=useState([]);
-  const ref = firebase.firestore().collection("user");
+  // const ref = firebase.firestore().collection("user");
 
-  const getChatData = () => {
-    ref.onSnapshot((querySnapShot) => {
-      const item = [];
-      querySnapShot.forEach((doc) => {
-        item.push(doc.data());
-      });
-      setData(item);
-    });
-  };
-  useEffect(() => {
-    getChatData();
-    console.log(data);
-  }, []);
+  // const getChatData = () => {
+  //   ref.onSnapshot((querySnapShot) => {
+  //     const item = [];
+  //     querySnapShot.forEach((doc) => {
+  //       item.push(doc.data());
+  //     });
+  //     setData(item);
+  //   });
+  // };
+  // useEffect(() => {
+  //   getChatData();
+  //   console.log(data);
+  // }, []);
 
   return (
     <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
