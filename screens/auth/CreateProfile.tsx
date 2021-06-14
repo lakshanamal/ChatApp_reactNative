5fa59b6b30fd5d801241037415782ffadffb35b0
lakshanamal <lakshanamal100@gmail.com>
2021-06-14 23:34:53 +0530

    bug fixed

--- screens/auth/CreateProfile.tsx
+++ screens/auth/CreateProfile.tsx
@@ -11,7 +11,7 @@ import Avater from "../../assets/images/avater.png";
 import * as ImagePicker from "expo-image-picker";
 const CreateProfile = () => {
   const [name, setName] = useState("");
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState("");
 
   const createProfile = () => {};
 
@@ -19,7 +19,7 @@ const CreateProfile = () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
     console.log(result);
@@ -42,13 +42,20 @@ const CreateProfile = () => {
       <Text>Hello lets make a profile</Text>
 
       <TouchableOpacity onPress={handleChoosePhoto}>
-        <Image
-          source={Avater}
-          style={{
-            width: 50,
-            height: 50,
-          }}
-        />
+        {image !== "" ? (
+          <Image
+            source={{ uri: image }}
+            style={{ width: 100, height: 100, borderRadius: 50 }}
+          />
+        ) : (
+          <Image
+            source={Avater}
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+        )}
       </TouchableOpacity>
       <TextInput
         style={{ marginVertical: 10, fontSize: 17 }}
@@ -57,12 +64,6 @@ const CreateProfile = () => {
         onChangeText={setName}
       />
       <Button title="Next" onPress={createProfile} />
-      {image && (
-        <Image
-          source={{ uri: image }}
-          style={{ width: 200, height: 200, borderRadius: 25% }}
-        />
-      )}
     </View>
   );
 };
