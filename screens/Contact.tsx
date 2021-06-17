8ead08d83c3ca4a9f6846a095046a3440dd14f41
lakshanamal <lakshanamal100@gmail.com>
2021-06-18 01:00:36 +0530

    bug fixed

--- screens/Contact.tsx
+++ screens/Contact.tsx
@@ -8,22 +8,36 @@ import firebase from "./../firebaseConfig";
 import { useEffect } from "react";
 
 export default function Contact() {
-  const [users, setUsers] = useState([]);
-  const getUsers = async () => {
-    await firebase.firestore().collection("users").get().then((snapshot)=>{
-      snapshot.forEach(doc=>{
-        if(doc.exists){
-          setUsers([...users,doc.data()])
-        }
-      })
-    });
-    
-  };
+  const [users, setUsers] = useState<Array<{}>>([]);
 
   useEffect(() => {
+    const getUsers = async () => {
+      await firebase
+        .firestore()
+        .collection("users")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            const user = doc.data();
+            console.log(user);
+            setUsers((prevState) => [...prevState, user]);
+          });
+        });
+      // .then((snapshot) => {
+      //   snapshot.forEach((doc) => {
+      //     if (doc.exists) {
+      //       console.log(doc.data());
+      //       setUsers([...users,doc.data()]);
+      //     }
+      //   });
+      // });
+      // console.log(userList);
+    };
     getUsers();
   }, []);
+
   console.log(users);
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -31,7 +45,6 @@ export default function Contact() {
         style={{ width: "100%" }}
         renderItem={({ item }) => <ContactListItem user={item} />}
       />
-
     </View>
   );
 }
