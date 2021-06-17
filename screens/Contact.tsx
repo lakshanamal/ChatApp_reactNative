651f2b3bf759ce293fe2ff21122be438671b87b9
lakshanamal <lakshanamal100@gmail.com>
2021-06-17 19:50:51 +0530

    get and set contact in contact menu

--- screens/Contact.tsx
+++ screens/Contact.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 import ContactListItem from "../components/ContactListItem";
-import NewMessageButton from "../components/NewMessageButton";
+// import NewMessageButton from "../components/NewMessageButton";
 import { View } from "../components/Themed";
-
-import users from "../data/Users";
+import firebase from "./../firebaseConfig";
+// import s from "../data/Users";
+import { useEffect } from "react";
 
 export default function Contact() {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    await firebase.firestore().collection("users").get().then((snapshot)=>{
+      snapshot.forEach(doc=>{
+        if(doc.exists){
+          setUsers([...users,doc.data()])
+        }
+      })
+    });
+    
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  console.log(users);
   return (
     <View style={styles.container}>
       <FlatList
@@ -14,7 +31,7 @@ export default function Contact() {
         style={{ width: "100%" }}
         renderItem={({ item }) => <ContactListItem user={item} />}
       />
-      {/* <NewMessageButton /> */}
+
     </View>
   );
 }
