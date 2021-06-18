9700a6ad9f78b8b9654cc9b448036eaf6249d472
lakshanamal <lakshanamal100@gmail.com>
2021-06-18 22:44:19 +0530

    bug fixed

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -11,22 +11,38 @@ export type ContactListItemProps = {
 };
 
 const ContactListItem = (props: ContactListItemProps) => {
-  const { user } = props;
   const navigation = useNavigation();
+  // user in the constact list
+  const { user } = props;
 
   const onClick = async () => {
     try {
+      // get current user
       const currentUserAuth = await firebase.auth().currentUser;
       const currentUser = await firebase
         .firestore()
         .collection("users")
         .where("id", "==", currentUserAuth?.uid)
         .get();
+
       const userData = currentUser.docs[0].data();
-      const ref = firebase.firestore().collection("chatrooms").doc();
- 
+
+      // // check chat room is exists
+      // const checkChatRoom = await firebase
+      //   .firestore()
+      //   .collection("chatrooms")
+      //   .get();
+
+      // checkChatRoom.docs.forEach((doc) => {
+      //   console.log(doc.data().user);
+      //   if(doc.data().user[0]. && doc.data().user[1])
+      // });
+
+      const chatRoomId = user.id + userData.id;
+      console.log(chatRoomId);
+      // create new chat room
       const chatRoom = {
-        id: ref.id,
+        id: chatRoomId,
         user: [
           {
             id: userData.id,
@@ -49,11 +65,10 @@ const ContactListItem = (props: ContactListItemProps) => {
         .then(() => {
           console.log("Chat room create sucess full");
         });
-      navigation.navigate("ChatRoom", { id: chatRoom.id, name: user.name });
+      // navigation.navigate("ChatRoom", { id: chatRoom.id, name: user.name });
     } catch (err) {
       console.log(err);
     }
-    // navigation.navigate("Contacts");
   };
 
   return (
--- screens/ChatsScreeen.tsx
+++ screens/ChatsScreeen.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 import ChatListItem from "../components/CharListItem";
 import NewMessageButton from "../components/NewMessageButton";
 import { View } from "../components/Themed";
-
+import firebase from "../firebaseConfig";
 import chatRooms from "../data/ChatRooms";
 
 export default function ChatsScreen() {
+  const getChatList = async () => {
+    // await firebase.firestore().doc()
+  };
+  useEffect(() => {
+    getChatList();
+  }, []);
+
   return (
     <View style={styles.container}>
       {/* <ChatListItem chatRoom={chatRooms[0]} /> */}
-      <FlatList data={chatRooms} 
-      style={{width:"100%"}}
-      renderItem={({item})=><ChatListItem chatRoom={item} />}
+      <FlatList
+        data={chatRooms}
+        style={{ width: "100%" }}
+        renderItem={({ item }) => <ChatListItem chatRoom={item} />}
       />
-      <NewMessageButton/>
-     
+      <NewMessageButton />
     </View>
   );
 }
