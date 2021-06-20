66092dbe53cae2970f44cbceaa6991c2950ca4ea
lakshanamal <lakshanamal100@gmail.com>
2021-06-21 00:52:31 +0530

    get message from database

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Text, View } from "../components/Themed";
-// import { useRoute } from "@react-navigation/native";
 import ChatRoomData from "../data/Chats";
 import { FlatList, ImageBackground } from "react-native";
 import ChatMessage from "../components/ChatMessage";
 import BG from "../assets/images/BG.png";
 import InputBox from "../components/InputBox";
-
+import firebase from "../firebaseConfig";
+import { useRoute } from "@react-navigation/native";
 
 const ChatRoomScreen = () => {
+  const route = useRoute([]);
+  const { id } = route.params;
+
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("chats")
+      .doc(id)
+      .get()
+      .then((docs) => {
+        console.log(docs.data().message);
+        setMessage(docs.data());
+      });
+  }, []);
+  console.log(message.message);
   return (
     <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
       <FlatList
@@ -17,7 +33,7 @@ const ChatRoomScreen = () => {
             // backgroundColor: "white",
           }
         }
-        data={ChatRoomData.messages}
+        data={message.message}
         inverted
         renderItem={({ item }) => <ChatMessage message={item} />}
       />
