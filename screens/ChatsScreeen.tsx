1ce737a25a7918367f011fb6367a203955213ba3
lakshanamal <lakshanamal100@gmail.com>
2021-06-29 21:04:07 +0530

    loading added to main page

--- screens/ChatsScreeen.tsx
+++ screens/ChatsScreeen.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, ImageBackground, StyleSheet } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
 import ChatListItem from "../components/CharListItem";
 import NewMessageButton from "../components/NewMessageButton";
 import { View, Text } from "../components/Themed";
@@ -14,11 +19,13 @@ export default function ChatsScreen() {
   const [isCurrentUser, setIsCurrentUser] = useState(0);
   const [currentUser, setCurrentUser] = useState<User>();
   const video = React.useRef(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getChatList = async () => {
       // const currentUserAuth = firebase.auth().currentUser;
       setChatList([]);
+      setLoading(true);
       firebase
         .firestore()
         .collection("users")
@@ -44,6 +51,7 @@ export default function ChatsScreen() {
                     }
 
                     setChatList((prev) => [...prev, docs.data()]);
+                    setLoading(false);
                   }
                 });
             }
@@ -52,54 +60,69 @@ export default function ChatsScreen() {
     };
     getChatList();
   }, []);
-
+  console.log(loading);
   return (
     <View style={{ backgroundColor: "#123858", width: "100%", height: "100%" }}>
-      <View style={styles.container}>
-        {chatList.length !== 0 ? (
-          <View style={styles.midContainer}>
-            <View
-              style={{
-                width: "100%",
-                height: "100%",
-                backgroundColor: "#16456D",
-                flex: 1,
-                alignItems: "center",
-              }}
-            >
-              <Text style={{ fontSize: 40 }}>Hi!</Text>
-              <Text style={{ fontSize: 50 }}>Welcome to Memo</Text>
-              <Video
-                ref={video}
-                style={{ width: 350, height: 350 }}
-                source={welcome}
-                resizeMode="contain"
-                shouldPlay={true}
-              />
+      {!loading ? (
+        <View style={styles.container}>
+          {chatList.length !== 0 ? (
+            <View style={styles.midContainer}>
+              <View
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  backgroundColor: "#16456D",
+                  flex: 1,
+                  alignItems: "center",
+                }}
+              >
+                <Text style={{ fontSize: 40 }}>Hi!</Text>
+                <Text style={{ fontSize: 50 }}>Welcome to Memo</Text>
+                <Video
+                  ref={video}
+                  style={{ width: 350, height: 350 }}
+                  source={welcome}
+                  resizeMode="contain"
+                  shouldPlay={true}
+                />
 
-              <NewMessageButton isStart={true} />
+                <NewMessageButton isStart={true} />
+              </View>
             </View>
-          </View>
-        ) : (
-          <View style={styles.midContainer}>
-            {chatList && (
-              <FlatList
-                data={chatList}
-                style={{ width: "100%" }}
-                renderItem={({ item }) => (
-                  <ChatListItem
-                    // key={item}
-                    chatRoom={item}
-                    isUser={isCurrentUser}
-                    currentUser={currentUser!}
-                  />
-                )}
-              />
-            )}
-            <NewMessageButton isStart={false} />
-          </View>
-        )}
-      </View>
+          ) : (
+            <View style={styles.midContainer}>
+              {chatList && (
+                <FlatList
+                  data={chatList}
+                  style={{ width: "100%" }}
+                  renderItem={({ item }) => (
+                    <ChatListItem
+                      // key={item}
+                      chatRoom={item}
+                      isUser={isCurrentUser}
+                      currentUser={currentUser!}
+                    />
+                  )}
+                />
+              )}
+              <NewMessageButton isStart={false} />
+            </View>
+          )}
+        </View>
+      ) : (
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "space-around",
+            display: "flex",
+            backgroundColor: "#16456D",
+          }}
+        >
+          <ActivityIndicator size="large" color="white" />
+        </View>
+      )}
     </View>
   );
 }
